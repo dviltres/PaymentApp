74cbd5df444b0ec812e532d92102c4ff53452248
dviltres18 <44000119+dviltres18@users.noreply.github.com>
2022-10-30 01:41:25 -0300

    Error ic_launcher_background icon

--- app/src/main/java/com/dviltres/paymentapp/presentation/MainActivity.kt
+++ app/src/main/java/com/dviltres/paymentapp/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.dviltres.paymentapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -21,10 +22,14 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    private val paymentViewModel: PaymentViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen().apply {
-
+            setKeepVisibleCondition {
+                paymentViewModel.state.isLoading
+            }
         }
         setContent {
             PaymentAppTheme {
